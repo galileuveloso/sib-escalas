40e1a7e21700ab5e82787236316c0ed3bfcc4704
galileuveloso <galileu.veloso@hotmail.com>
2022-04-28 22:49:56 -0300

    crud de Funcoes e Pessoas

---
+++ SibEscalas.Dominio/Extensions/Store.cs
@@ -0,0 +1,17 @@
+﻿using SibEscalas.Dominio.Models;
+using System.Collections.Generic;
+
+namespace SibEscalas.Dominio.Extensions
+{
+    public static class Store
+    {
+        public static IList<Funcao> Funcoes { get; set; }
+        public static IList<Pessoa> Pessoas { get; set; }
+
+        public static void Iniciar()
+        {
+            Funcoes = new List<Funcao>();
+            Pessoas = new List<Pessoa>();
+        }
+    }
+}
---
+++ SibEscalas.Dominio/Servico/ArquivoServico.cs
@@ -0,0 +1,30 @@
+﻿using SibEscalas.Dominio.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SibEscalas.Dominio.Servico
+{
+    public class ArquivoServico
+    {
+        public void SalvarArquivo(string stringPath, string titulo)
+        {
+            //recebo caminho do arquivo -> lista de objetos
+            //converto a lista pra Json
+            //escrevo no disco
+
+            string local = stringPath + titulo + ".json";
+
+            using (StreamWriter escritor = new StreamWriter(local))
+            {
+                //escritor.WriteLine(JsonExtension.ConverteObjectParaJSon<IList<Pessoa>>(pessoas));
+            }
+        }
+
+        public void AbrirArquivo()
+        {
+
+        }
+    }
+}
